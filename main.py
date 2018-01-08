97114a1019b00ffad9e42ac1bfbd27a142a93166
Codescu Radu <radu.codescu27@gmail.com>
2018-01-09 00:09:46 +0200

    Weather

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import socket
 import datetime
 import webbrowser
 import os
+import requests
 from gtts import gTTS
 
 yaml_data = []
@@ -67,7 +68,12 @@ def show_weather(city):
     weather_url = "http://api.openweathermap.org/data/2.5/weather?q="+city
     weather_url = weather_url + "&APPID=" + weather_key
     r = requests.get(weather_url)
-    say(r)
+    print("Weather: " + str(r.text))
+
+
+def search_on_map(list_to_search):
+    place = ' '.join(map(str, list_to_search))
+    webbrowser.open('https://www.google.ro/maps/place/' + place)
 
 
 def execute(voice_string, r):
@@ -98,8 +104,12 @@ def execute(voice_string, r):
         search_on_youtube(lst)
         return
     if voice_string.split()[0] == yaml_data['WEATHER']:
-        city='+'.join(map(str, voice_string.split()[1:]))
+        city = '+'.join(map(str, voice_string.split()[1:]))
         show_weather(city)
+    if ' '.join(map(str, voice_string.split()[0:2])) == yaml_data['MAP']:
+        lst = voice_string.split()[2:]
+        search_on_map(lst)
+        return
 
 
 def main():
@@ -108,9 +118,9 @@ def main():
         yaml_data = yaml.load(f)
 
     r = sr.Recognizer()
-    r.pause_threshold = 0.6  # seconds of non-speaking audio before a phrase is considered complete
+    r.pause_threshold = 0.4  # seconds of non-speaking audio before a phrase is considered complete
     r.phrase_threshold = 0.3  # minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
-    r.non_speaking_duration = 0.4  # seconds of non-speaking audio to keep on both sides of the recording
+    r.non_speaking_duration = 0.2  # seconds of non-speaking audio to keep on both sides of the recording
     r.energy_threshold = 110000
     r.dynamic_energy_adjustment_damping = 0.15
     while True:
